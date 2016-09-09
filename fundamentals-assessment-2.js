8eabe71db4b603cca30798f5f447a8b4a2b29112
Vanessa Nava <vnava26@gmail.com>
2016-09-08 20:50:29 -0700

    added fundamentals assessment

--- fundamentals-assessment-2.js
+++ fundamentals-assessment-2.js
@@ -2,3 +2,35 @@ var two = 2;
 var myArray = [1, two, "three", "4", 5, "six", "7", 8];
 
 //YOUR CODE BELOW
+var PrintAll = function(arrayNumbers){
+for (var i = 0; i < myArray.length; i++){
+	console.log(printAll(myArray));
+}
+
+};
+
+var longArray = function(arrayElements){
+	if(arrayElements > 7){
+
+	}
+
+	return true;
+
+	else {
+		return false;
+	};
+
+var countNums = function(counterElement){
+
+}
+
+var counter = 0;
+
+for (var type in countNums){
+	if(countNums === number){
+var countNums.type++;
+	console.log(counter.type);
+
+	}
+
+};
